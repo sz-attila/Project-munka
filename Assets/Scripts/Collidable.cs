59e33a8b2c7816e0521dba4aea10d6ca40bc50aa
sz-attila <szalai.attila2002@gmail.com>
2021-11-05 14:56:41 +0100

    Combat system

--- Assets/Scripts/Collidable.cs
+++ Assets/Scripts/Collidable.cs
@@ -33,7 +33,7 @@ public class Collidable : MonoBehaviour
 
     protected virtual void OnCollide(Collider2D col)
     {
-        Debug.Log(col.name);
+        Debug.Log("OnCollide was not implemented in " + this.name);
     }
 }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,41 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : Mover
 {
-    private BoxCollider2D boxCollider;
-    private Vector3 moveDelta;
-    private RaycastHit2D hit;
-    private void Start() 
-    {
-        boxCollider = GetComponent<BoxCollider2D>();    
-    }
-
-    //movement
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
-    
-        moveDelta = new Vector3(x,y,0);
-        if(moveDelta.x>0)
-            transform.localScale = Vector3.one;
-        else if(moveDelta.x<0)
-            transform.localScale = new Vector3(-1,1,1);
 
-        hit = Physics2D.BoxCast(transform.position,boxCollider.size, 0, new Vector2(0,moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Hero","Block"));
-        if (hit.collider == null)
-        {
-        transform.Translate(0,moveDelta.y * Time.deltaTime,0);
-
-        }
-        hit = Physics2D.BoxCast(transform.position,boxCollider.size, 0, new Vector2(moveDelta.x,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Hero","Block"));
-        if (hit.collider == null)
-        {
-        transform.Translate(moveDelta.x * Time.deltaTime,0,0);
-        
-        }
-
-
+        UpdateMotor(new Vector3(x,y,0));
     }
 }
